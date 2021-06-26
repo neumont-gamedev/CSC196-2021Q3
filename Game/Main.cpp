d0e610ee12c8706a9e2e89aa74e6e83082f290f8
neumont-gamedev <rmaple@neumont.edu>
2021-06-26 11:18:18 -0600

    Added core library

---
+++ Game/Main.cpp
@@ -0,0 +1,24 @@
+#include "core.h"
+#include <iostream>
+
+bool Update(float dt)
+{
+	return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
+	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+}
+
+int main()
+{
+	char name[] = "CSC196";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
+
+	Core::GameLoop();
+	Core::Shutdown();
+}
