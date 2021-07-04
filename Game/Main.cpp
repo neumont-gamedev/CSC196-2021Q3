3af2b58f8bb60ac1714d3a3a17e8de5af73be164
neumont-gamedev <rmaple@neumont.edu>
2021-07-04 11:16:09 -0600

    class 070221 - particle system firework

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -2,6 +2,7 @@
 #include "Math/Vector2.h"
 #include "Math/Color.h"
 #include "Graphics/Shape.h"
+#include "Graphics/ParticleSystem.h"
 #include <iostream>
 #include <vector>
 
@@ -10,16 +11,26 @@ std::vector<nc::Vector2> points = { { -5, -5 }, { 5, -5 }, { 0, 5 }, { -5, -5 } 
 nc::Shape shape{ points, nc::Color{ 0, 1, 0 } };
 const float speed = 5;
 float timer = 0;
+nc::ParticleSystem particleSystem;
 
 bool Update(float dt)
 {
 	bool quit = Core::Input::IsPressed(Core::Input::KEY_ESCAPE);
 
 	timer += dt * 5;
-	//int x, y;
-	//Core::Input::GetMousePos(x, y);
-	//position.x = static_cast<float>(x);
-	//position.y = static_cast<float>(y);
+	
+	if (Core::Input::IsPressed(Core::Input::BUTTON_LEFT))
+	{
+		int x, y;
+		Core::Input::GetMousePos(x, y);
+		nc::Vector2 psPosition;
+		psPosition.x = static_cast<float>(x);
+		psPosition.y = static_cast<float>(y);
+
+		std::vector<nc::Color> colors = { nc::Color{1, 1, 1}, nc::Color{1, 0, 0}, nc::Color{0, 1, 0} };
+		particleSystem.Create(psPosition, 150, 2, colors[rand() % colors.size()], 150);
+	}
+	particleSystem.Update(dt);
 
 	nc::Vector2 input;
 	if (Core::Input::IsPressed('A')) input.x = -1;
@@ -36,6 +47,7 @@ void Draw(Core::Graphics& graphics)
 {
 	float scale = 1 + (std::sin(timer) + 1) * 2;
 	shape.Draw(graphics, position);
+	particleSystem.Draw(graphics);
 }
 
 int main()
@@ -45,6 +57,10 @@ int main()
 	Core::RegisterUpdateFn(Update);
 	Core::RegisterDrawFn(Draw);
 
+	particleSystem.Startup();
+
 	Core::GameLoop();
 	Core::Shutdown();
+
+	particleSystem.Shutdown();
 }
